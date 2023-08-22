a961bdd8b70edfbf06eddf56b6db72e8ac132f74
Alvin Yau <ayaualvin@gmail.com>
2023-08-22 17:36:39 +0900

    seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,49 +1,97 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
 require 'faker'
 
 # Cleaning the database
 puts "clearing DB"
 
+#Destroying the database\
 User.destroy_all
 HealthRecord.destroy_all
 Appointment.destroy_all
 Medicine.destroy_all
+Reminder.destroy_all
+Support.destroy_all
 
 # Creating the database
 puts "creating DB"
 
-# Creating the users
-gary = User.new(username: "gary", address: "507-12 Uchikanna, Chita gun taketoyo cho, Aichi ken", email: "toppiggary@hotmail.com", password: "123456")
-gary.save!
-ritsuki = User.new(username: "ritsuki", address: "58-18 Katsumotocho nakafure, Iki shi, Nagasaki ken", email: "ricky0912t@gmail.com", password: "123456")
-ritsuki.save!
-james = User.new(username: "james", address: "12-13 Ogawa, Tone gun minakami machi, Gumma ken", email: "hkjl87@hotmail.co.uk", password: "123456")
-james.save!
-alvin = User.new(username: "alvin", address: "356-3 Muyacho kizu, Naruto shi, Tokushima ken", email: "ayaualvin@gmail.com", password: "123456")
+# Creating the users (elderly)
+alvin = User.new(
+  email: "ayaualvin@gmail.com",
+  password: "123456",
+  first_name: "ken",
+  last_name: "yau",
+  gender: "male",
+  address: "G/F, 20 Spring Garden Ln, Wan Chai, Hong Kong",
+  phone_number: "9123 3678",
+  birth_date: "22/12/1945",
+  height: rand(140..200),
+  latitude: -90 + (180 * rand),
+  longtitude: -180 + (360 * rand)
+)
 alvin.save!
 
-5.times do
-  user = User.new(
-    email: Faker::Internet.email,
-    first_name: Faker::Name.first_name,
-    last_name: Faker::Name.last_name,
-    address: Faker::Address.full_address,
-    phone_number: Faker::PhoneNumber.cell_phone,
-    birth_date: Faker::Date.birthday(min_age: 65, max_age: 95)
-    height: rand(140..200)
-    latitude: rand
-    longtitude:
-  )
-  user.save!
-end
+james = User.new(
+  email: "hkjl87@hotmail.co.uk",
+  password: "123456",
+  first_name: "james",
+  last_name: "lau",
+  gender: "male",
+  address: "Big Ben Westminster, London SW1A 0AA, UK",
+  phone_number: "9123 3678",
+  birth_date: "22/12/1945",
+  height: rand(140..200),
+  latitude: -90 + (180 * rand),
+  longtitude: -180 + (360 * rand)
+)
+james.save!
+
+# Creating the users (caregivers)
+ritsuki = User.new(
+  email: "ricky0912t@gmail.com",
+  password: "123456",
+  first_name: "ritsuki",
+  last_name: "toshima",
+  gender: "male",
+  address: "2 Chome-11-3 Meguro, Meguro City, Tokyo 153-0063",
+  phone_number: "080-9290-8767"
+)
+ritsuki.save!
 
 puts "created #{User.count} users"
+
+# ------------------------------------------------------------------------------- #
+
+# Creating the medicines
+puts "Creating Medicines"
+
+alvin_medicine = Medicine.new(
+    name: "Diuretic",
+    description: "Help reduce fluid buildup in the body.",
+    dosage: 2,
+    dosage_remaining: 230,
+    unit: "tablets",
+    start_date: DateTime.now - Random.rand(10..24),
+    end_date: DateTime.now + Random.rand(365..730),
+    frequency: 2,
+    user_id: alvin.id
+  )
+alvin_medicine.save!
+
+
+
+
+
+
+# Creating the taken_records
+3.times do
+  (1..20).each do |medicine_id|
+    TakenRecord.create!(medicine_id: medicine_id)
+  end
+end
+
+
+
+
 
 # Creating the health records
 20.times do
@@ -72,27 +120,4 @@ puts "Creating Appointments"
     user_id:
   )
   appointment.save!
-end
-
-# Creating the medicines
-puts "Creating Medicines"
-20.times do
-  medicine = Medicine.new(
-    name: rand(1..5),
-    description: rand(45..70),
-    dosage: rand(70..110),
-    units_remaining: rand(120..160),
-    start_date: DateTime.now - Random.rand(10..24),
-    end_date: DateTime.now - Random.rand(1..9),
-    time_to_take:,
-    user_id:
-  )
-  medicine.save!
-end
-
-# Creating the taken_records
-3.times do
-  (1..20).each do |medicine_id|
-    TakenRecord.create!(medicine_id: medicine_id)
-  end
 end
