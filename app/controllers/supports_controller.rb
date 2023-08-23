db2d7d7186d25085ef7b2f1612c1fee0c36d258d
James Lau <hkjl87@hotmail.co.uk>
2023-08-23 15:18:21 +0900

    fixed show path

--- app/controllers/supports_controller.rb
+++ app/controllers/supports_controller.rb
@@ -1,12 +1,13 @@
 class SupportsController < ApplicationController
   def index
-    @elderlies = current_user.elderlies
+    # @elderlies = current_user.elderlies
     # @trusted_users = current_user.supports_as_trusted_user
-    @supports = policy_scope(Support)
+    @supports = policy_scope(Support).where(trusted_user: current_user)
   end
 
   def show
-    @support = current_user.supports_as_elderly.find(params[:id])
+    @support = current_user.supports_as_trusted_user.find(params[:id])
     authorize @support
   end
+
 end
