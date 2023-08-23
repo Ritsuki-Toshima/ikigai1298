8546cc8377772cd62e44c6d94b76770ea9765800
James Lau <hkjl87@hotmail.co.uk>
2023-08-23 11:07:17 +0900

    updated supports controller

--- app/controllers/supports_controller.rb
+++ app/controllers/supports_controller.rb
@@ -2,9 +2,11 @@ class SupportsController < ApplicationController
   def index
     @elderlies = current_user.supports_as_elderly
     @trusted_users = current_user.supports_as_trusted_user
+    @supports = policy_scope(Support)
   end
 
   def show
     @support = current_user.supports_as_elderly.find(params[:id])
+    authorize @support
   end
 end
