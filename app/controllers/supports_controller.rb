c2fefdfc012cf33bfdd35f3354d54ffb27b2db05
Gary Topping <toppinggary@hotmail.com>
2023-08-23 13:52:30 +0900

    show page for elderly and pundit update

--- app/controllers/supports_controller.rb
+++ app/controllers/supports_controller.rb
@@ -1,10 +1,11 @@
 class SupportsController < ApplicationController
-def index
-@elderlies = current_user.supports_as_elderly
-@trusted_users = current_user.supports_as_trusted_user
-end
+  def index
+    @elderlies = current_user.supports_as_elderly
+    @trusted_users = current_user.supports_as_trusted_user
+  end
 
-def show
-  @support = current_user.supports_as_elderly.find(params[:id])
-end
+  def show
+    @support = current_user.supports_as_trusted_user.find(params[:id])
+    authorize @support
+  end
 end
