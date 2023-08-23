2b925ad981c3bb3adc87bca9f3cfc8a6771d1fca
James Lau <hkjl87@hotmail.co.uk>
2023-08-23 17:37:02 +0900

    checkpoint

--- app/controllers/supports_controller.rb
+++ app/controllers/supports_controller.rb
@@ -1,10 +1,9 @@
 class SupportsController < ApplicationController
   def index
+    @users = User.all
     # @elderlies = current_user.elderlies
     # @trusted_users = current_user.supports_as_trusted_user
     @supports = policy_scope(Support).where(trusted_user: current_user)
-    @supports = @supports.search(params[:search][:query]) if params[:search].present?
-
   end
 
   def show
