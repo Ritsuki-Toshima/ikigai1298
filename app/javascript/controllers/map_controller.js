b2540d68b0c6766eb8108b8295f570e06f80659a
Gary Topping <toppinggary@hotmail.com>
2023-08-24 12:41:24 +0900

    maps working for appointments index and show pages

--- app/javascript/controllers/map_controller.js
+++ app/javascript/controllers/map_controller.js
@@ -5,7 +5,8 @@ import mapboxgl from 'mapbox-gl' // Don't forget this!
 export default class extends Controller {
   static values = {
     apiKey: String,
-    markers: Array
+    markers: Array,
+    marker: Object
   }
 
   connect() {
@@ -15,9 +16,17 @@ export default class extends Controller {
       container: this.element,
       style: "mapbox://styles/mapbox/streets-v10"
     })
+    console.log("This is the marker value");
+    console.log(this.markerValue);
+    console.log(Object.keys(this.markerValue).length === 0);
+    if (Object.keys(this.markerValue).length === 0) {
+      this.#addMarkersToMap()
+      this.#fitMapToMarkers()
+    } else {
+      this.#addMarkerToMap(this.markerValue);
+      this.#fitMapToMarker(this.markerValue);
+    }
 
-    this.#addMarkersToMap()
-    this.#fitMapToMarkers()
   }
 
   #addMarkersToMap() {
@@ -35,9 +44,28 @@ export default class extends Controller {
     })
   }
 
+  #addMarkerToMap(marker) {
+    const popup = new mapboxgl.Popup().setHTML(marker.info_window_html)
+
+    // Create a HTML element for your custom marker
+    const customMarker = document.createElement("div")
+    customMarker.innerHTML = marker.marker_html
+
+    new mapboxgl.Marker(customMarker)
+      .setLngLat([marker.lng, marker.lat])
+      .setPopup(popup)
+      .addTo(this.map)
+  }
+
   #fitMapToMarkers() {
     const bounds = new mapboxgl.LngLatBounds()
     this.markersValue.forEach(marker => bounds.extend([marker.lng, marker.lat]))
+    this.map.fitBounds(bounds, { padding: 70, maxZoom: 15, duration: 0 })
+  }
+
+  #fitMapToMarker(marker) {
+    const bounds = new mapboxgl.LngLatBounds()
+    bounds.extend([marker.lng, marker.lat])
     this.map.fitBounds(bounds, { padding: 70, maxZoom: 15, duration: 0 })
   }
 }
