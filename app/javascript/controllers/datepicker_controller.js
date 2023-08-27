c99db4a2423836de8a2508824959a936df0a03d5
James Lau <hkjl87@hotmail.co.uk>
2023-08-27 19:56:22 +0900

    checkpoint

--- app/javascript/controllers/datepicker_controller.js
+++ app/javascript/controllers/datepicker_controller.js
@@ -1,10 +1,28 @@
-import { Controller } from "@hotwired/stimulus"
+// import { Controller } from "@hotwired/stimulus"
 
-// Connects to data-controller="datepicker"
+// // Connects to data-controller="datepicker"
+// export default class extends Controller {
+//   connect() {
+//     console.log(this);
+//     flatpickr(this.datePickerTarget, { minDate: "today" })
+//     // flatpickr(".anotherSelector");
+//   }
+// }
+
+import { Controller } from "@hotwired/stimulus"
+import flatpickr from "flatpickr";
+
 export default class extends Controller {
+  static targets = ["datePickerStart", "datePickerEnd"];
+
+
   connect() {
     console.log(this);
-    flatpickr(this.datePickerTarget, { minDate: "today" })
+    flatpickr(this.datePickerStartTarget, {minDate: "today"})
+    flatpickr(this.datePickerEndTarget, {minDate: "today"})
+
     // flatpickr(".anotherSelector");
   }
+
+
 }
