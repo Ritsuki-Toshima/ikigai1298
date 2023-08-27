04c7dc9d58888c4360a62fedaf6a0673fb1695a1
Gary Topping <toppinggary@hotmail.com>
2023-08-27 18:58:10 +0900

    small progress

--- app/notifications/comment_notification.rb
+++ app/notifications/comment_notification.rb
@@ -1,29 +1,44 @@
 # To deliver this notification:
 #
 # CommentNotification.with(post: @post).deliver_later(current_user)
-# CommentNotification.with(post: @post).deliver(current_user)
+# CommentNotification.with(post: @post).deliver_later(User.all)
+CommentNotification.with(post: @post).deliver(current_user)
 
 class CommentNotification < Noticed::Base
   # Add your delivery methods
   #
-  deliver_by :database
-  deliver_by :action_cable
+  deliver_by :database, format: :to_database, debug: true
+  deliver_by :action_cable, debug: true  # format: :to_action_cable
   # deliver_by :email, mailer: "UserMailer"
   # deliver_by :slack
   # deliver_by :custom, class: "MyDeliveryMethod"
 
   # Add required params
   #
-  # param :post
+  param :post
 
   # Define helper methods to make rendering easier.
   #
+  def to_database
+    {
+    type: self.class.name,
+    params: params,
+    account: Current.account,
+    }
+  end
+
+  # def to_action_cable
+  #   {
+
+  #   }
+  # end
+
   def message
-    t("Gary created this test notification")
+    t(".message")
   end
 
   def url
-    # post_path(params[:post])
     Rails.application.routes.default_url_options[:host] = 'localhost:3000'
+    post_path(params[:post])
   end
 end
