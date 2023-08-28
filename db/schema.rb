4885a6ba8bb1776716a591b1a4455c6873cd3855
Ritsuki Toshima <ricky0912t@gmail.com>
2023-08-28 15:14:57 +0900

    yeah

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_08_23_084910) do
+ActiveRecord::Schema[7.0].define(version: 2023_08_28_045938) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -65,6 +65,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_23_084910) do
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.date "date"
     t.index ["user_id"], name: "index_health_records_on_user_id"
   end
 
