02a72b9f552daca0168da4dc8d33613c3565ddc0
Alvin Yau <ayaualvin@gmail.com>
2023-08-29 13:08:19 +0900

    support-index-noti

--- db/schema.rb
+++ db/schema.rb
@@ -65,6 +65,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_28_094709) do
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text "ocr_text"
     t.date "date"
     t.index ["user_id"], name: "index_health_records_on_user_id"
   end
@@ -83,6 +84,14 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_28_094709) do
     t.datetime "updated_at", null: false
     t.string "status", default: "pending"
     t.index ["user_id"], name: "index_medicines_on_user_id"
+  end
+
+  create_table "posts", force: :cascade do |t|
+    t.string "title"
+    t.text "body"
+    t.text "image_description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "reminders", force: :cascade do |t|
