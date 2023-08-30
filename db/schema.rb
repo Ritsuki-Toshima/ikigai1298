0efd4a9a7c1d7c128cf69e151f3427ad149eea08
Alvin Yau <ayaualvin@gmail.com>
2023-08-31 00:29:33 +0900

    support-pages-linkages

--- db/schema.rb
+++ db/schema.rb
@@ -99,14 +99,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_29_062327) do
     t.index ["recipient_type", "recipient_id"], name: "index_notifications_on_recipient"
   end
 
-  create_table "posts", force: :cascade do |t|
-    t.string "title"
-    t.text "body"
-    t.text "image_description"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "reminders", force: :cascade do |t|
     t.bigint "medicine_id", null: false
     t.datetime "time_to_taken"
